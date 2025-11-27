db2471ccddc3b4a9e6711cc20940f9aafc881e69
santiagomj13 <asmejiatorres@alumnat.iessacolomina.es>
2025-11-27 11:29:20 +0100

    Add files via upload

---
+++ prova-js/push.js
@@ -0,0 +1,8 @@
+// Variables
+let fruitesEstiu = ["meló", "albercoc"];
+// Execució
+// Aquí mostra només els dos valors inicials
+console.log(fruitesEstiu);
+// Afegim "nectarina" a la nostra llista
+fruitesEstiu.push("nectarina");
+console.log(fruitesEstiu);
