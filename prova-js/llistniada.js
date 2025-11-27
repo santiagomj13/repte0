a0d3f1165277ab35e76e0912551243fe1625d828
santiagomj13 <asmejiatorres@alumnat.iessacolomina.es>
2025-11-27 11:38:10 +0100

    Add files via upload

---
+++ prova-js/llistniada.js
@@ -0,0 +1,8 @@
+// Variables
+let llistaNumerada = [1, 2, 3, [100, 200]];
+// Execució
+// Aquí mostra només els valors de la subllista
+console.log(llistaNumerada[3]);
+// Afegim el número "300" dins la subllista
+llistaNumerada[3].push(300);
+console.log(llistaNumerada);
